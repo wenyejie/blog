578e616f7356ea2abaefc4b94829f98f0b8e9950
wenyejie <wenyejie@swipal.com>
2019-10-31 10:56:51 +0800

    相关测试

---
+++ assets/codeHighlight.js
@@ -0,0 +1,9 @@
+/**
+ * @author: Storm
+ * @email: wenyejie@foxmail.com
+ */
+
+const $view = document.querySelector('code[data-code-highlight="javascript"]')
+const $code = document.querySelector('script[data-code-highlight="javascript"]')
+
+$view.innerHTML = $code.innerText
